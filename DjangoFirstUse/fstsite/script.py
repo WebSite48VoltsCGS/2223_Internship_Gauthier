718adfdaf8ff47e99f3f09b7ff675cb86feaffd7
Minorzar <gauthier_biehler@yahoo.com>
2023-06-28 17:25:24 +0200

    Day 8 commit: Working on the tex to pdf part. Now everything is automatically made. Need to work on the writing of the file then on the admin page to be able to generate the pdf with gestion of the reduction, etc

--- DjangoFirstUse/fstsite/script.py
+++ DjangoFirstUse/fstsite/script.py
@@ -3,6 +3,7 @@
 import pylatex
 import os
 import django
+import subprocess
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'fstsite.settings'
 
@@ -30,55 +31,125 @@ Commandes = models.Commande
 packs = ["babel", "units", "bera", "graphicx", "fancyhdr", "fp", "longtable", "marvosym", "array", "multirow",
          "makecell", "datetime", "numprint"]
 
-bid = Doc(documentclass='article', lmodern=False, textcomp=False, geometry_options="a4paper")
+bid = Doc(documentclass='article', document_options=["11pt", "french"], lmodern=False,
+          textcomp=False, geometry_options="a4paper")
+
+adder = bid.append
+
 preamble = bid.preamble
-adder = bid.preamble.append
+adderPreamble = bid.preamble.append
+
+preamblePath = r"BasicTemplate\preamble.txt"
+endPath = r"BasicTemplate\endfile.txt"
+headerPath = r"BasicTemplate\introdoc.txt"
+
 
 
 for pack in packs:
     bid.packages.append(Pack(pack))
 
 
-# Function definition
-
-def V48(bid):
-    adder(noEscape("\n"))
-    adder(noEscape(r"\def\societe{48Volts}"))
-    adder(noEscape(r"\def\adresseM{7 chemin des libellules ~-~ 95800"))
-    adder(noEscape(r"\textsc{Courdimanche}}"))
-    adder(noEscape(r"\def\adresse21{7 chemin des libellules \\ 95800"))
-    adder(noEscape(r"\textsc{Courdimanche}}"))
-    adder(noEscape(r"\def\capital{7 000 €}"))
-    adder(noEscape(r"\def\gerant{Nicolas \textsc{Papazoglou}}"))
-    adder(noEscape(r"\def\tel{06 09 28 15 36}"))
-    adder(noEscape(r"\def\mail{nicolas.papazoglou@48-volts.fr}"))
-    adder(noEscape(r"\def\contact{\gerant \\ Tel : \tel \\ Mail : \mail}"))
-    adder(noEscape(r"\def\lieu{\textsc{Courdimanche}}"))
-    adder(noEscape(r"\def\RCS{801 880 055 00018}"))
-    adder(noEscape(r"\def\web{www.48-volts.fr}"))
-
-def variable(bid):
-    adder(noEscape("\n"))
-    adder(noEscape(r"\def\TVA{20}"))
-    adder(noEscape(r"\def\TotalHT{0}"))
-    adder(noEscape(r"\def\ssTotalHT{0}"))
-    adder(noEscape(r"\def\TotalTVA{0}"))
-    adder(noEscape(r"\def\part_name{}"))
-    adder(noEscape(r"\def\ListeProduits{}"))
-
-
+TVA = 20
+TotalHT = 0
+ssTotalHT = 0
+TotalTVA = 0
+produits = []       # list of list, 0 -> Article.product + Article.model
+                    #               1 -> Commande.number
+                    #               2 -> Article.buying_price
+                    #               3 -> Article.discount default = 0
+                    #               4 -> Commande.coeff default = 1
+                    #               5 -> Total
 
 client = Clients.objects.get(user_name="Hervé")
 vente = Ventes.objects.get(id_client=client.id)
 
-
-bid.append(noEscape(client))
-bid.append(noEscape(vente))
-for cmd in vente.id_commande.all():
-    bid.append(cmd)
-
-V48(bid)
-variable(bid)
+bidNumber = vente.id_bid
 
 
-bid.generate_tex(filepath=r"D:\Git\Projet\2223_Internship_Gauthier\DjangoFirstUse\fstsite\devis\devis")
+cmdBash = r"pdflatex D:\Git\Projet\2223_Internship_Gauthier\DjangoFirstUse\fstsite\devis\devis.tex"
+
+# Function definition
+
+
+def init_list():
+    global produits
+    for commands in vente.id_commande.all():
+        produit = [commands.article.product + " " + commands.article.model + " " + commands.article.marque,
+                   commands.number, commands.article.buying_price, 0, 1, 0]
+        produits.append(produit)
+
+
+def V48():
+    # gérer l'acquisation de: nomPrestation, lieuPrestation, debutPrestation, finPrestation, ClientNom, ClientAdresse
+    adderPreamble(noEscape(r"\def\devisNum{" + str(bidNumber) + "}"))
+    with open(preamblePath, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    for line in lines:
+        adderPreamble(noEscape(str(line.replace('\n', "", 1))))
+
+
+def bank():
+    with open(endPath, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    for line in lines:
+        adder(noEscape(str(line.replace('\n', "", 1))))
+
+
+def header():
+    with open(headerPath, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    for line in lines:
+        adder(noEscape(str(line.replace('\n', "", 1))))
+
+
+def calcul():
+    global ssTotalHT, TotalHT, produits
+    for produit in produits:
+        total = produit[1]*produit[2]*produit[4]*(1-produit[3]/100)
+        ssTotalHT += total
+        TotalHT += total
+        produit[5] = total
+
+
+def createTable():
+    global produits, ssTotalHT, TotalHT
+
+def writing():
+    # Preamble
+    V48()
+
+    # Header
+    header()
+
+    # Bid
+
+    init_list()
+    calcul()
+    bid.append(noEscape(client))
+    bid.append(noEscape(vente))
+    for cmd in vente.id_commande.all():
+        adder(cmd)
+
+    # Bank
+    bank()
+
+    # Compilation
+    bid.generate_tex(filepath=r"D:\Git\Projet\2223_Internship_Gauthier\DjangoFirstUse\fstsite\devis\devis")
+
+
+
+    process = subprocess.Popen(cmdBash, shell=True,
+                            cwd=r"D:\Git\Projet\2223_Internship_Gauthier\DjangoFirstUse\fstsite\devis",
+                            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    process.communicate()
+
+
+    process = subprocess.Popen(cmdBash, shell=True,
+                            cwd=r"D:\Git\Projet\2223_Internship_Gauthier\DjangoFirstUse\fstsite\devis",
+                            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    process.communicate()
+
+# writing()
