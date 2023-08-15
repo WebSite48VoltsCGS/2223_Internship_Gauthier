225e2cb1547336e7e7d348f1fbd1116f7444ce2b
Minorzar <gauthier_biehler@yahoo.com>
2023-08-15 14:30:06 +0200

    Clear some useless file, prepare to create a dev branch that will be easier to work on

--- fstsite/fstsite/settings.py
+++ fstsite/fstsite/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "polls.apps.PollsConfig",
     "sql_test.apps.SqlTestConfig",
     'django.contrib.admin',
     'django.contrib.auth',
