e78b28c08e4fb1c15c99f91c6e8de61b9d5232eb
Minorzar <gauthier_biehler@yahoo.com>
2023-08-28 17:42:17 +0200

    The file generation is now done as well as the downloading. What's next to do is to continue working on the validation and admin part for the database. Then to migrate the code on the real server and modify the path of the code

--- fstsite/sql_test/views.py
+++ fstsite/sql_test/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, FileResponse
 from django.views import generic
 from django.shortcuts import render
 
 from .models import Article, Client, Command
+from .script import writing
+import os
 
 
 # Macro and variable
@@ -49,18 +51,42 @@ class CommandeView(generic.DetailView):
         return Command.objects.order_by("article")
 
 def devis(request):
-    global prestationLoc, prestationName, debPrestation, finPrestation
-
     command = Command.objects.all()
+    comm_id = None
+
+    if request.GET.get("billing_id"):
+        comm_id = request.GET.get("billing_id")
 
     if request.method == 'POST':
+
+        directory = f"D:\\Git\Projet\\2223_Internship_Gauthier\\fstsite\\devis"
+        for filename in os.listdir(directory):
+            if filename.lower().endswith('.png'):
+                continue
+            file_path = os.path.join(directory, filename)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+                print(f"Deleted: {file_path}")
+
         prestationName = request.POST.get('nomPrestation')
         prestationLoc = request.POST.get('lieuPrestation')
         debPrestation = request.POST.get('debPrestation')
         finPrestation = request.POST.get('finPrestation')
+        bidId = request.POST.get('selected_value')
+        coeff = request.POST.getlist('coeff_undefined')
+        discount = request.POST.getlist('discount_undefined')
+        deposit = request.POST.get('Deposit')
+
+        writing(name=prestationName, loc=prestationLoc, deb=debPrestation, fin=finPrestation, deposit=deposit,
+                discount=discount, coeff=coeff, bidId=bidId)
+
+        file_path = f"D:\\Git\Projet\\2223_Internship_Gauthier\\fstsite\\devis\\devis_{bidId}.pdf"
+        response = FileResponse(open(file_path, 'rb'))
+        response['Content-Disposition'] = f'attachment; filename="devis_{bidId}.pdf"'
+
+        return response
 
     elif request.method == 'GET':
-        comm_id = request.GET.get('billing_id')
         if comm_id:
             articles = Article.objects.filter(commandline__command__billing_id=comm_id)
             articles_data = [{'name': article.product, 'value': article.is_multiple} for article in articles]
